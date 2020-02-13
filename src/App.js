67f6080988fd1f749b3a3289ab2c83113f442afe
Ruthie Byers <ruthie@sendwave.com>
2020-02-12 19:41:43 -0500

    Add an interval exercise controller that chooses a random interval
    From a  very short  list of intervals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import IntervalExercise  from './IntervalExercise.js'
+import IntervalExerciseController  from './IntervalExerciseController.js'
 
 import logo from './logo.svg';
 import './App.css';
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <IntervalExercise buttonTitle="click me!"/>
+      <IntervalExerciseController/>
     </div>
   );
 }
---
+++ src/IntervalExerciseController.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Teoria from "teoria";
+
+import IntervalExercise from "./IntervalExercise.js";
+
+const POSSIBLE_INTERVALS = [
+    Teoria.interval('M2'),
+    Teoria.interval('m3'),
+]
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+class IntervalExerciseController extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            interval: POSSIBLE_INTERVALS[getRandomInt(POSSIBLE_INTERVALS.length)]
+        }
+    }
+
+    render() {
+        return <IntervalExercise buttonTitle='hello'/>
+    }
+}
+
+export default IntervalExerciseController
