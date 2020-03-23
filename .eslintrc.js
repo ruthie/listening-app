999c8373630518bbc282c7c2fe2d553b50a363b6
Ruthie Byers <ruthie@alum.mit.edu>
2020-03-22 20:51:19 -0400

    Add ESlist

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"extends": "airbnb",
+
+    "rules": {
+		"indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "semi": "off",
+        "react/destructuring-assignment": "off",
+        "object-curly-newline": "off",
+        "react/button-has-type": "off",
+	}
+}
