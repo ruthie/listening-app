2083c0320e33175626bd17e7b35f8d02f4f3b36e
Ruthie Byers <ruthie@alum.mit.edu>
2020-03-22 21:28:55 -0400

    Use friendlier names on answer buttons

--- src/IntervalExercise.js
+++ src/IntervalExercise.js
@@ -17,6 +17,20 @@ function PlaySoundButton({ onClick }) {
     )
 }
 
+const friendlyIntervalNames = {
+    'm2': 'Minor 2nd',
+    'M2': 'Major 2nd',
+    'm3': 'Minor 3rd',
+    'M3': 'Major 3rd',
+    'P4': 'Perfect 4th',
+    'd5': 'Diminished 5th',
+    'P5': 'Perfect 5th',
+    'm6': 'Minor 6th',
+    'M6': 'Major 6th',
+    'm7': 'Minor 7th',
+    'M7': 'Major 7th'
+}
+
 class IntervalExercise extends Component {
     constructor() {
         super()
@@ -43,7 +57,8 @@ class IntervalExercise extends Component {
             <div>
                 <PlaySoundButton onClick={this.playInterval}/>
                 {possibleAnswers.map(x => {
-                    var intervalName = x.toString()
+                    let intervalName = x.toString()
+                    var intervalFriendlyName = friendlyIntervalNames[intervalName]
 
                     var color = 'white'
                     if (submittedAnswers.includes(x.toString())) {
@@ -53,7 +68,7 @@ class IntervalExercise extends Component {
                     return (
                         <AnswerButton
                             key={intervalName}
-                            text={intervalName}
+                            text={intervalFriendlyName}
                             value={intervalName}
                             color={color}
                             onClick={onAnswerClick}
