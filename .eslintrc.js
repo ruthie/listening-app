d6369ed975b07cdb4daaa40a27a6ad74f315c456
Ruthie Byers <ruthie@alum.mit.edu>
2020-04-05 10:41:13 -0400

    Fix indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-    "indent": ["error", 4],
+        "indent": ["error", 4],
         "react/jsx-indent": ["error", 4],
         "react/jsx-indent-props": ["error", 4],
 
