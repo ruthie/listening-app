b7490cc5fddfd486fbaccafd6f2283de1e7fda90
Ruthie Byers <ruthie@alum.mit.edu>
2020-04-23 21:22:40 -0400

    Move some styles to the app component from the quiz component
    So they also apply to the quiz completed page.  Might have to move some of these around again if I want a wider home screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import QuizController from './QuizController'
 
+import './App.css';
+
 function App() {
     return (
-        <div className="App">
+        <div className="app">
             <QuizController />
         </div>
     );
