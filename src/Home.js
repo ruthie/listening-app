775d773f7eb03b75f44ee1f39461d524fcb9db22
Harris Lapiroff <harris@freedom.press>
2020-08-08 17:19:53 -0400

    Fix CSS bug where homepage icons were not scaling equally

--- src/Home.js
+++ src/Home.js
@@ -17,61 +17,63 @@ class Home extends Component {
                 </div>
 
                 <div className="max-width content">
-                    <div>
-                        <div className="three-exercise-container">
-                            <Link
-                                to="/upward-intervals"
-                                className="exercise-link"
-                            >
-                                <CircleIcon color="blue" symbol="upward" />
-                                <div> Upward Interval Identification</div>
-                            </Link>
-                            <Link
-                                to="/downward-intervals"
-                                className="exercise-link"
-                            >
-                                <CircleIcon color="blue" symbol="downward" />
-                                <div> Downward Interval Identification</div>
-                            </Link>
-                            <Link
-                                to="/simultaneous-intervals"
-                                className="exercise-link"
-                            >
-                                <CircleIcon color="blue" symbol="simultaneous" />
-                                <div> Simultaneous Interval Identification</div>
-                            </Link>
-                        </div>
-                        <div className="three-exercise-container">
-                            <Link
-                                to="/upward-interval-tuning"
-                                className="exercise-link"
-                            >
-                                <CircleIcon color="green" symbol="upward" />
-                                <div> Upward Interval Tuning</div>
-                            </Link>
-                            <Link
-                                to="/downward-interval-tuning"
-                                className="exercise-link"
-                            >
-                                <CircleIcon color="green" symbol="downward" />
-                                <div> Downward Interval Tuning</div>
-                            </Link>
-                            <Link
-                                to="/simultaneous-interval-tuning"
-                                className="exercise-link"
-                            >
-                                <CircleIcon color="green" symbol="simultaneous" />
-                                <div> Simultaneous Interval Tuning</div>
-                            </Link>
-                        </div>
+
+                    <div className="three-exercise-container">
+                        <Link
+                            to="/upward-intervals"
+                            className="exercise-link"
+                        >
+                            <CircleIcon color="blue" symbol="upward" />
+                            <div> Upward Interval Identification</div>
+                        </Link>
+                        <Link
+                            to="/downward-intervals"
+                            className="exercise-link"
+                        >
+                            <CircleIcon color="blue" symbol="downward" />
+                            <div> Downward Interval Identification</div>
+                        </Link>
+                        <Link
+                            to="/simultaneous-intervals"
+                            className="exercise-link"
+                        >
+                            <CircleIcon color="blue" symbol="simultaneous" />
+                            <div> Simultaneous Interval Identification</div>
+                        </Link>
                     </div>
+
+                    <div className="three-exercise-container">
+                        <Link
+                            to="/upward-interval-tuning"
+                            className="exercise-link"
+                        >
+                            <CircleIcon color="green" symbol="upward" />
+                            <div> Upward Interval Tuning</div>
+                        </Link>
+                        <Link
+                            to="/downward-interval-tuning"
+                            className="exercise-link"
+                        >
+                            <CircleIcon color="green" symbol="downward" />
+                            <div> Downward Interval Tuning</div>
+                        </Link>
+                        <Link
+                            to="/simultaneous-interval-tuning"
+                            className="exercise-link"
+                        >
+                            <CircleIcon color="green" symbol="simultaneous" />
+                            <div> Simultaneous Interval Tuning</div>
+                        </Link>
+                    </div>
+
                     <a
                         href="https://github.com/ruthie/listening-app"
                         className="source-link"
                     >
                         Source Code
                     </a>
-                </div>            </div>
+                </div>
+            </div>
         )
     }
 }
